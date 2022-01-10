966ffc170234e515c3c62042163f75fbe8a89586
Meghan Pund <meghan.pund@gmail.com>
2022-01-10 15:34:59 -0500

    ~f~ strings instead of messy concat strings

--- marathon_calc.py
+++ marathon_calc.py
@@ -7,13 +7,13 @@ def completion_time(pace_mins, pace_secs, distance_in_miles):
     hours = floor((total_secs / 60) / 60)
          
     if distance_in_miles == 13.1:
-        print("Your projected half marathon time is " + str(hours) + " hours, " + str(minutes) + " minutes, and " + str(seconds) + " seconds!")
+        print(f"Your projected half marathon time is {hours} hours, {minutes} minutes, and {seconds} seconds!")
     elif distance_in_miles == 26.2:
-        print("Your projected marathon time is " + str(hours) + " hours, " + str(minutes) + " minutes, and " + str(seconds) + " seconds!")
+        print(f"Your projected marathon time is {hours} hours, {minutes} minutes, and {seconds} seconds!")
     elif distance_in_miles >= 26.2:
-        print("Your projected ultramarathon time is " + str(hours) + " hours, " + str(minutes) + " minutes, and " + str(seconds) + " seconds!")
+        print(f"Your projected ultramarathon time is {hours} hours, {minutes} minutes, and {seconds} seconds!")
     else:
-        print("Your projected finish time is " + str(hours) + " hours, " + str(minutes) + " minutes, and " + str(seconds) + " seconds!")
+        print(f"Your projected finish time is {hours} hours, {minutes} minutes, and {seconds} seconds!")
 
 completion_time(9, 20, 13.1)
 
@@ -22,13 +22,13 @@ def pace(race_hours, race_minutes, race_seconds, distance_in_miles):
     pace_mins = floor(total_secs / 60 / distance_in_miles)
     pace_secs = floor((total_secs % (pace_mins * 60 * distance_in_miles)) / distance_in_miles)
     if distance_in_miles == 13.1:
-        print("To run a " + str(race_hours) + ":" + str(race_minutes) + ":" + str(race_seconds) + " half marathon, you need to clock " + str(pace_mins) + "-minute, " + str(pace_secs) + "-second miles!")    
+        print(f"To run a {race_hours}:{race_minutes}:{race_seconds} half marathon, you need to clock {pace_mins}-minute, {pace_secs}-second miles!")    
     elif distance_in_miles == 26.2:
-        print("To run a " + str(race_hours) + ":" + str(race_minutes) + ":" + str(race_seconds) + " marathon, you need to clock " + str(pace_mins) + "-minute, " + str(pace_secs) + "-second miles!")
+        print(f"To run a {race_hours}:{race_minutes}:{race_seconds} marathon, you need to clock {pace_mins}-minute, {pace_secs}-second miles!")
     elif distance_in_miles >= 26.2:
-        print("To run a " + str(race_hours) + ":" + str(race_minutes) + ":" + str(race_seconds) + " ultramarathon, you need to clock " + str(pace_mins) + "-minute, " + str(pace_secs) + "-second miles!")
+        print(f"To run a {race_hours}:{race_minutes}:{race_seconds} ultramarathon, you need to clock {pace_mins}-minute, {pace_secs}-second miles!")
     else:
-        print("To run a " + str(race_hours) + ":" + str(race_minutes) + ":" + str(race_seconds) + " race, you need to clock " + str(pace_mins) + "-minute, " + str(pace_secs) + "-second miles!")
+        print(f"To run a {race_hours}:{race_minutes}:{race_seconds} race, you need to clock {pace_mins}-minute, {pace_secs}-second miles!")
     
 
 pace(2, 0, 0, 13.1)
